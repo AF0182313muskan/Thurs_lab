2308af46245a910cbd7a872b45c9719d9254744e
Muskan Shaikh <105120711+AF0182313muskan@users.noreply.github.com>
2022-08-26 16:04:40 +0530

    16-08-22

--- LabSession3.java
+++ LabSession3.java
@@ -1,23 +1,18 @@
 package practicals;
 
 import java.util.Scanner;
-class LabSession
-		{
-		public static void main(String args[])
-		{
-		Scanner sc=new Scanner(System.in);
-		String name1,name2,name3;
-		char name4,name5;
-		System.out.println("Enter your first name:");
-		name1=sc.next();
-		System.out.println("Enter the middle name:");
-		name2=sc.next();
-		System.out.println("Enter the last naem:");
-		name3=sc.next();
-		System.out.println(name1+ " " +name2+ " " +name3);
-		name4=name1.charAt(0);
-		name5=name2.charAt(0);
-		System.out.println(name4+ "."+name5+ "." +name3);
-		}
-	}
-
+public class LabSession3
+			{
+				public static void main(String args[])
+				{
+				Scanner sc=new Scanner(System.in);
+				System.out.println("Enter the String:");
+				String s=sc.next();
+				System.out.println("Enter the character you want to count:");
+				String so=sc.next();
+				int count1=s.length();
+				int count2=s.replace(so,"").length();
+				int count=count1-count2;
+				System.out.println("The number of occurance of:" +so+ " is" +count);
+				}
+			}
