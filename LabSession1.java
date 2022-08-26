46b84773c3923f47b4c90b5bc8c7100880dde80a
Muskan Shaikh <105120711+AF0182313muskan@users.noreply.github.com>
2022-08-26 16:09:47 +0530

    18-08-22

---
+++ LabSession1.java
@@ -0,0 +1,23 @@
+package practicals;
+
+import java.util.Scanner;
+class LabSession1
+		{
+		public static void main(String args[])
+		{
+		Scanner sc=new Scanner(System.in);
+		String name1,name2,name3;
+		char name4,name5;
+		System.out.println("Enter your first name:");
+		name1=sc.next();
+		System.out.println("Enter the middle name:");
+		name2=sc.next();
+		System.out.println("Enter the last naem:");
+		name3=sc.next();
+		System.out.println(name1+ " " +name2+ " " +name3);
+		name4=name1.charAt(0);
+		name5=name2.charAt(0);
+		System.out.println(name4+ "."+name5+ "." +name3);
+		}
+	}
+
